927ca6c78dbb75134ca4b1efe0abdb016de2f72d
foundationguardian26 <foundationguardian26@gmail.com>
2026-06-09 08:31:32 -0500

    Fixing git ignore and cleaning up repo

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Is2gDDeN40okwzDXUpSuHH8SnMpe8Co7yAtjyk1IHMY=\"\n}"
--- .next/dev/static/chunks/app_dashboard_sop_[id]_page_tsx_1-7dtlk._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_0zl_i28._.js","static/chunks/node_modules_02-r35g._.js"],
-    source: "dynamic"
-});
